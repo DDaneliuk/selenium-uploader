b006426f15b37f1f9c0495fd2e5da0a7fa545197
Denys Daneliuk <denys.d@digitalidea.studio>
2023-01-10 16:17:18 +0200

    config

---
+++ config.py
@@ -0,0 +1,27 @@
+keys = {
+    'key1' : 'must',
+    'key2' : 'chief',
+    'key3' : 'cover',
+    'key4' : 'security',
+    'key5' : 'alert',
+    'key6' : 'draft',
+    'key7' : 'like',
+    'key8' : 'earth',
+    'key9' : 'hawk',
+    'key10' : 'age',
+    'key11' : 'palm',
+    'key12' : 'palm',
+}
+password = 'Щдгю13-21Вощд-339'
+
+opensea = {
+    'main': 'https://opensea.io',
+    'account': 'https://opensea.io/account',
+    'create':  'https://opensea.io/asset/create'
+}
+
+# 1 - Start from - 1. Range - 10. Total - 10
+# 2 - Start from - 11. Range - 100. Total - 110
+# 3 - Start from - 111. Range - 1. Total - 111
+# 4 - Start from - 111. Range - 49. Total - 159
+# 5 - Start from - 160. Range - 40. Total - 200
