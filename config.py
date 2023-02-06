9c3063b77573575896bf15e3feb33b9e57e07f90
Denys Daneliuk <denys.d@digitalidea.studio>
2023-02-06 21:45:47 +0200

    add bot crend

--- config.py
+++ config.py
@@ -20,6 +20,10 @@ opensea = {
     'create':  'https://opensea.io/asset/create'
 }
 
+bot_token = "5573016646:AAFi5PBkD9p3ojG9XVpkbKV16zGOXhY5Yic"
+bot_id="434249851"
+bot_art="428742525"
+
 # 1 - Start from - 1. Range - 10. Total - 10
 # 2 - Start from - 11. Range - 100. Total - 110
 # 3 - Start from - 111. Range - 1. Total - 111
